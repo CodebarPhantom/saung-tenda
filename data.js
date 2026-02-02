3466c9884c298823ac057a0f9c9b262764a580d0
CodebarPhantom <eryangamer10@gmail.com>
2026-02-02 16:35:58 +0700

    [chore] embed url

--- data.js
+++ data.js
@@ -27,7 +27,7 @@ const umkmData = {
     whatsappMessage: "Assalamu’alaikum, mau pesan ayam/ikan bakar.",
     whatsappCTAMessage: "Halo! Saya mau info menu dan order ayam/ikan bakar.",
     mapsUrl: "https://maps.google.com/maps?q=Saung+Tenda+Ayam+%26+Ikan+Nila+Panatayudha+2+Karawang", 
-    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.999999!2d107.282000!3d-6.336000!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e697c0000000001%3A0x0000000000000000!2sSaung%20Tenda%20Ayam%20%26%20Ikan%20Nila!5e0!3m2!1sid!2sid!4v1700000000000", 
+    mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.679135902015!2d107.30081057457635!3d-6.305820061711298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6977c2fff00141%3A0x16a9478a4415bcbf!2sSaung%20Tenda%20Ayam%20%26%20Ikan%20Nila%20(Bakar%20-%20Cobek)%20-%20Depan%20Sop%20Janda!5e0!3m2!1sen!2sid!4v1770024866086!5m2!1sen!2sid", 
     address: "Jl. Panatayuda I, Nagasari, Kec. Karawang Bar., Karawang, Jawa Barat 41314" 
   },
 
